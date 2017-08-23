199addf8b4944fa8cc1c7ede5e0ad2f181f3d5b0
Douglas Feuser <douglas.feuser@gmail.com>
2017-08-23 09:30:40 -0300

    divide

---
+++ modulo01/aula01/divide.js
@@ -0,0 +1,22 @@
+const inverse = (x) => x * -1
+
+const add = (y) => (x) => x + y
+const subtract = (y) => (x) => add(inverse(y))(x)
+
+const divide = (y) => (x) => {
+
+    let result = y
+    let cont = 0
+
+    const sub = subtract(x)
+
+   	while (result > 0) {
+        result = sub(result)
+        cont++
+    }
+
+    return cont
+}
+
+console.log('12 / 6 =', divide(12)(6))
+console.log('24 / 2 =', divide(24)(2))
