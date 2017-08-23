5ae4f54ec64999cb84c893dd8afd4c7f4c050a39
Douglas Feuser <douglas.feuser@gmail.com>
2017-08-23 08:34:03 -0300

    add and subtract

--- modulo01/aula01/add.js
+++ modulo01/aula01/add.js
@@ -4,11 +4,9 @@ const inverse = (x) => x * -1
 const add = (y) => (x) => x + y
 const subtract = (y) => (x) => add(inverse(y)) (x)
 
-console.log(add(4)(6))
-console.log(subtract(4)(6))
+console.log('Adição:', add(4)(6))
+console.log('Subtração', subtract(4)(6))
 
-//const PA = (y) => (x) => x + y
-
-//const PA_razao2 = PA(2)
-
-//console.log(PA_razao2(5))
+const PA = (y) => (x) => x + y
+const PA_razao2 = PA(2)
+console.log('PA: ', PA_razao2(5))
